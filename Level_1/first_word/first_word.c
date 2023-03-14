648b416b4ba6f01aa589368dc3bda477b12585e1
Mucahit Polat <mupolat@k1m25s06.42istanbul.com.tr>
2023-03-14 16:40:08 +0300

    EXAM

---
+++ Level_1/first_word/first_word.c
@@ -0,0 +1,23 @@
+#include    <unistd.h>
+
+int main(int ac, char **av)
+{
+    int i = 0;
+
+    if (ac == 2)
+    {
+        while (av[1][i] == 32 || av[1][i] == 9)
+            i++;
+        while ((av[1][i] != 32 && av[1][i] != 9) && av[1][i])
+        {
+            write(1, &av[1][i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+}
+
+/*
+Bu programda bizden girdiğimiz argümanlar arasından sadece ilk kelimeyi alıp
+yazdırmamızı istiyot
+*/
---
+++ Level_1/search_and_replace/search_and_replace.c
@@ -0,0 +1,8 @@
+#include    <unistd.h>
+
+int main(int ac, char **av)
+{
+    int i = 0;
+
+    if (ac == 4 && !av[2][1] & )
+}
